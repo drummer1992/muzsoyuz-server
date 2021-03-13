c028bad885a273f38b901648084282992422cd6b
Andrii_Varlamov <andrii.varlamov@backendless.consulting>
2021-03-13 09:35:59 +0200

    - code refactoring

--- app/api/context/decorators/service/index.js
+++ app/api/context/decorators/service/index.js
@@ -1,5 +1,7 @@
+import { ENV } from '../../../../config'
+
 export function Service(name) {
   return function(Clazz) {
-    Clazz.serviceRegExp = new RegExp(name)
+    Clazz.serviceRegExp = new RegExp(`^${ENV.API_PREFIX}${name}`)
   }
 }
--- app/main.js
+++ app/main.js
@@ -1,7 +1,6 @@
 import { NotFoundError } from './errors'
 import { StatusCode as c } from './constants/http'
 import fs from 'fs'
-import { ENV } from './config'
 import { predicates as p } from './utils/object'
 
 const services = fs.readdirSync('./app/api')
@@ -9,8 +8,6 @@ const services = fs.readdirSync('./app/api')
   .map(module => require(`./api/${module}`))
 
 const serialize = response => p.isUndefined(response) ? '' : JSON.stringify(response)
-const hasApiPrefix = url => url.startsWith(ENV.API_PREFIX)
-const trimApiPrefix = url => url.replace(ENV.API_PREFIX, '')
 
 /**
  *
@@ -22,17 +19,13 @@ const main = async (req, res) => {
   let response = new NotFoundError('Service Not Found')
   let statusCode = c.NOT_FOUND
 
-  if (hasApiPrefix(req.url)) {
-    req.url = trimApiPrefix(req.url)
+  const Service = services.find(Service => Service.isProperlyService(req.url))
 
-    const Service = services.find(Service => Service.isProperlyService(req.url))
+  if (Service) {
+    const service = new Service(req, res)
 
-    if (Service) {
-      const service = new Service(req, res)
-
-      response = await service.execute()
-      statusCode = service.response.statusCode
-    }
+    response = await service.execute()
+    statusCode = service.response.statusCode
   }
 
   const serializedResponse = serialize(response)
