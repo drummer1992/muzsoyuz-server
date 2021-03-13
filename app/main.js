8543f4c5f6ef6ca19e9cc67bbcebe4df88976386
Andrii_Varlamov <andrii.varlamov@backendless.consulting>
2021-03-13 19:02:48 +0200

    - code refactoring

--- app/main.js
+++ app/main.js
@@ -2,6 +2,7 @@ import { NotFoundError } from './errors'
 import { StatusCode as c } from './constants/http'
 import fs from 'fs'
 import { predicates as p } from './utils/object'
+import { withCors } from './utils/http/with-cors'
 
 const services = fs.readdirSync('./app/api')
   .filter(item => item.includes('.js'))
@@ -15,35 +16,26 @@ const serialize = response => p.isUndefined(response) ? '' : JSON.stringify(resp
  * @param {ServerResponse} res
  * @returns {Promise<void>}
  */
-const main = async (req, res) => {
+const main = withCors(async (req, res) => {
   let response
   let statusCode
 
-  if (req.method === 'OPTIONS') {
-    statusCode = c.NO_CONTENT
+  const Service = services.find(Service => Service.isProperlyService(req.url))
+
+  if (Service) {
+    const service = new Service(req, res)
+
+    response = await service.execute()
+    statusCode = service.response.statusCode
   } else {
-    const Service = services.find(Service => Service.isProperlyService(req.url))
-
-    if (Service) {
-      const service = new Service(req, res)
-
-      response = await service.execute()
-      statusCode = service.response.statusCode
-    } else {
-      response = new NotFoundError('Service Not Found')
-      statusCode = c.NOT_FOUND
-    }
+    response = new NotFoundError('Service Not Found')
+    statusCode = c.NOT_FOUND
   }
 
   const serializedResponse = serialize(response)
 
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Request-Method', '*')
-  res.setHeader('Access-Control-Allow-Headers', '*')
-  res.setHeader('Access-Control-Allow-Methods', '*')
-
   res.writeHead(statusCode)
   res.end(serializedResponse)
-}
+})
 
 export default main
---
+++ app/utils/http/with-cors.js
@@ -0,0 +1,16 @@
+import { StatusCode as c } from '../../constants/http'
+
+export const withCors = main => function(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Request-Method', '*')
+  res.setHeader('Access-Control-Allow-Headers', '*')
+  res.setHeader('Access-Control-Allow-Methods', '*')
+
+  if (req.method === 'OPTIONS') {
+    res.writeHead(c.NO_CONTENT)
+
+    return res.end()
+  }
+
+  return main(req, res)
+}
