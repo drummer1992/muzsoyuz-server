58a9b414372b3016354e851268ce496ff006b73b
Andrii_Varlamov <andrii.varlamov@backendless.consulting>
2021-03-13 22:51:55 +0200

    - improved logging#5

--- app/api/context/decorators/logger.js
+++ app/api/context/decorators/logger.js
@@ -15,7 +15,11 @@ const loggerDecorator = fn => async function(...args) {
   }
 
   const time = new Date().toLocaleTimeString()
-  const body = `\n${JSON.stringify(this.request.body, null, 2)}`
+
+  const body =  Object.keys(this.request.body || {}).length
+    ? `\n${JSON.stringify(this.request.body, null, 2)}`
+    : ''
+
   const endpoint = `[${this.response.statusCode}] '${this.request.method} ${this.request.url}'`
 
   logger(`[${time}] ${endpoint}${errorMessage} ${ms} ms${body}`)
