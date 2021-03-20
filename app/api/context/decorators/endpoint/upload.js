d1b591c604c9c1b15988c7429670d78ac246c61d
Andrii_Varlamov <andrii.varlamov@backendless.consulting>
2021-03-20 18:02:49 +0200

    [user] - make dir for each user

--- app/api/context/decorators/endpoint/upload.js
+++ app/api/context/decorators/endpoint/upload.js
@@ -1,6 +1,9 @@
 import assert from 'assert'
+import fs from 'fs/promises'
 import { uploadFile } from '../../../../utils/http/upload'
 import { ENV as e } from '../../../../config'
+
+const makeDirIfNotExists = path => fs.mkdir(path).catch(console.error)
 
 export function UploadPipe(options) {
   options = options || { directory: e.STATIC_FOLDER }
@@ -15,15 +18,17 @@ export function UploadPipe(options) {
       assert(this.request.pathParams, 'pathParams hasn`t parsed')
       assert(this.request.pathParams.fileName, 'pathParams.fileName is required')
 
+      const pathToDir = `${e.PATH_TO_STATIC + options.directory}/${this.getCurrentUserId()}`
+
+      await makeDirIfNotExists(pathToDir)
+
       const fileName = this.request.pathParams.fileName
 
-      const filePath = `${e.PATH_TO_STATIC + options.directory}/${fileName}`
-
-      await uploadFile(this._req, filePath)
+      await uploadFile(this._req, `${pathToDir}/${fileName}`)
 
       return endpoint.call(this, {
         ...data,
-        fileURL: `${e.STATIC_SERVER + options.directory}/${fileName}`,
+        fileURL: `${e.STATIC_SERVER + options.directory}/${this.getCurrentUserId()}/${fileName}`,
       })
     }
 
