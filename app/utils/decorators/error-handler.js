ecde57a031c0e2fde9a01d1f5a59f3691c537a02
Andrii_Varlamov <andrii.varlamov@backendless.consulting>
2021-05-01 01:55:11 +0300

    - verbose errors

--- app/utils/decorators/error-handler.js
+++ app/utils/decorators/error-handler.js
@@ -4,13 +4,13 @@ const errorHandlerDecorator = fn => async function(...args) {
   try {
     return await fn.apply(this, args)
   } catch (error) {
+    console.error(error.message)
+    
     if (!(error instanceof APIError)) {
       error = new InternalServerError(`[UNHANDLED ERROR]: ${error.message}`)
     }
 
     if (error instanceof InternalServerError) {
-      console.error(error.stack)
-
       error.message = 'Internal Server Error'
     }
 
