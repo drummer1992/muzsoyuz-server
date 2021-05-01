eb05f4c43efec0a11e85e2815b8a42d09fd08419
Andrii_Varlamov <andrii.varlamov@backendless.consulting>
2021-05-01 11:19:31 +0300

    - added query logging

--- app/workflows/api/user/offers/find.js
+++ app/workflows/api/user/offers/find.js
@@ -3,6 +3,8 @@ import resolveQuery from '../../../user/resolve-query'
 const findOffers = query => {
   const resolvedQuery = resolveQuery(query)
 
+  console.log('QUERY', resolvedQuery)
+
   return Offer.find(resolvedQuery.where, resolvedQuery.props)
     .sort(resolvedQuery.orderBy)
     .limit(resolvedQuery.limit)
